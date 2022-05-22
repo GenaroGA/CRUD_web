8290debfcd5f9e4fa0939d64d6a5261ac904b3e4
Genaro Garcia <genaroenp2@gmail.com>
2022-05-22 18:37:25 -0500

    Poniendo orden

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
-
-
+#El INDEX: En el mostraremos la salida de las vistas al usuario y tambiên a traves de el enviaremos las distintas acciones que el usuario envie al controlador.
 require_once "controladores/plantilla.controlador.php";
 
 $plantilla = new ControladorPlantilla();
