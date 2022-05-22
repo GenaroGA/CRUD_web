6faf88aeef9ffbd177cf803ccd663debf7ec9728
Fernando <89424056+AtreyuLodbrok@users.noreply.github.com>
2022-05-22 15:40:03 -0500

    Add files via upload
    Primer controlador para la plantilla

---
+++ controladores/plantilla.controlador.php
@@ -0,0 +1,9 @@
+<?php
+
+class ControladorPlantilla{
+    /*Llamada a la plantilla*/
+    public function crtTraerPlantilla(){
+    #include() Se utiliza para invocar el archivo que contioene codigo php o hmtl
+    include "vistas/plantilla.php";
+    }
+}
